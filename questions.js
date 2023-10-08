19905cbc3123e649720649a9beb1bb516bad5188
Remiiiiii <tuf09783@temple.edu>
2023-10-08 19:05:52 -0400

    first commit

---
+++ questions.js
@@ -0,0 +1,33 @@
+let questions = [
+  {
+    numb: 1,
+    question:
+      "Management within your organization has defined a use case to support the confidentiality of data stored in a database. Which fo the following solutions will BEST  meet this need?",
+    answer: "skdlf;jas",
+    options: ["A. lkjls;ksjf", "B. ksjdf", "C. skdlf;jas", "D. sk"],
+  },
+  {
+    numb: 2,
+    question: "",
+    answer: "",
+    options: [],
+  },
+  {
+    numb: 3,
+    question: "",
+    answer: "",
+    options: [],
+  },
+  {
+    numb: 4,
+    question: "",
+    answer: "",
+    options: [],
+  },
+  {
+    numb: 5,
+    question: "",
+    answer: "",
+    options: [],
+  },
+];
