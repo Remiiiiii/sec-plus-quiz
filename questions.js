5f0714ec94f91bc1a046baf856c302865eb42845
Remiiiiii <tuf09783@temple.edu>
2023-10-09 15:42:29 -0400

    second commit

--- questions.js
+++ questions.js
@@ -3,31 +3,50 @@ let questions = [
     numb: 1,
     question:
       "Management within your organization has defined a use case to support the confidentiality of data stored in a database. Which fo the following solutions will BEST  meet this need?",
-    answer: "skdlf;jas",
-    options: ["A. lkjls;ksjf", "B. ksjdf", "C. skdlf;jas", "D. sk"],
+    answer: "Encryption",
+    options: [
+      "A. Hashing",
+      "B. Disk redundancies",
+      "C. Encryption",
+      "D. Patching",
+    ],
   },
   {
     numb: 2,
-    question: "",
-    answer: "",
-    options: [],
+    question:
+      "Apu manages network devices in his store and maintains copies of the configuration files for all the managed routers and switches. On a weekly basis, he creates hashes for these files and compares them with hashes he created on the same files the previous week. Which of the following use case is he MOST likely supporting?",
+    answer: "Supporting integrity",
+    options: [
+      "A. Supporting confidentiality",
+      "B. Supporting integrity",
+      "C. Supporting encryption",
+      "D. Supporing availability",
+    ],
   },
   {
     numb: 3,
-    question: "",
-    answer: "",
-    options: [],
+    question:
+      "Which of the following is a cryptographic algorithm that will create a fixed-length output froma a data file but cannot be used to re-create the original data file?",
+    answer: "MD5",
+    options: ["A. MD5", "B. AES", "C. IDS", "D. SIEM"],
   },
   {
     numb: 4,
-    question: "",
-    answer: "",
-    options: [],
+    question:
+      "Your organization hosts an e-commerce web server selling digital products. The server randomly experiences a high volume of sales and usage, which causes spikes in resource usage. These spikes occassionally take the server down. Which of the following should be implemented to prevent these outages",
+    answer: "Elasticity",
+    options: [
+      "A. Elasticity",
+      "B. Scalibility",
+      "C. Normalization",
+      "D. Stored Procedure",
+    ],
   },
   {
     numb: 5,
-    question: "",
-    answer: "",
-    options: [],
+    question:
+      "An administrator recently installed an IDS to help reduce the impact of security incidents. Which of the following BEST identifies the control type of an IDS?",
+    answer: "Detecive",
+    options: ["A. Preventive", "B. Physical", "C. Deterrent", "D. Detecive"],
   },
 ];
