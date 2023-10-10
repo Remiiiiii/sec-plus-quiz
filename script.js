9dd4e9984ddb1f9af083018884e40218fae6efc4
Remiiiiii <tuf09783@temple.edu>
2023-10-09 22:28:02 -0400

    fourth commit

--- script.js
+++ script.js
@@ -5,24 +5,67 @@ const main = document.querySelector(".main");
 const continueBtn = document.querySelector(".continue-btn");
 const quizSection = document.querySelector(".quiz-section");
 const quizBox = document.querySelector(".quiz-box");
+const resultBox = document.querySelector(".result-box");
+const tryAgainBtn = document.querySelector(".tryAgain-btn");
+const homeBtn = document.querySelector(".goHome-btn");
+const audio = document.getElementById("audio");
+const tryAgainAudio = document.getElementById("tryagain-audio");
+const scoreAudio = document.getElementById("score-audio");
+const homeAudio = document.getElementById("home-audio");
+const bgAudio = document.getElementById("bg-audio");
 
 startBtn.onclick = () => {
+  bgAudio.play();
+  audio.play();
   popupInfo.classList.add("active");
   main.classList.add("active");
 };
 
 exitBtn.onclick = () => {
+  audio.play();
   popupInfo.classList.remove("active");
   main.classList.remove("active");
 };
 
 continueBtn.onclick = () => {
+  audio.play();
   quizSection.classList.add("active");
   popupInfo.classList.remove("active");
   main.classList.remove("active");
   quizBox.classList.add("active");
   showQuestions(0);
   questionCounter(1);
+  headerScore();
+};
+
+tryAgainBtn.onclick = () => {
+  quizBox.classList.add("active");
+  nextBtn.classList.remove("active");
+  resultBox.classList.remove("active");
+
+  questionCount = 0;
+  questionNumb = 1;
+  userScore = 0;
+
+  showQuestions(questionCount);
+  questionCounter(questionNumb);
+  headerScore();
+  tryAgainAudio.play();
+};
+
+homeBtn.onclick = () => {
+  homeAudio.play();
+  quizSection.classList.remove("active");
+  nextBtn.classList.remove("active");
+  resultBox.classList.remove("active");
+
+  questionCount = 0;
+  questionNumb = 1;
+  userScore = 0;
+
+  showQuestions(questionCount);
+  questionCounter(questionNumb);
+
   headerScore();
 };
 
@@ -39,8 +82,10 @@ nextBtn.onclick = () => {
 
     questionNumb++;
     questionCounter(questionNumb);
+
+    nextBtn.classList.add("active");
   } else {
-    console.log("Question Completed");
+    showResultBox();
   }
 };
 
@@ -87,6 +132,8 @@ function optionSelected(answer) {
   for (let i = 0; i < allOptions; i++) {
     optionList.children[i].classList.add("disabled");
   }
+
+  nextBtn.classList.add("active");
 }
 
 function questionCounter(index) {
@@ -97,4 +144,30 @@ function questionCounter(index) {
 function headerScore() {
   const headerScoreText = document.querySelector(".header-score");
   headerScoreText.textContent = `Score: ${userScore} / ${questions.length}`;
+}
+
+function showResultBox() {
+  scoreAudio.play();
+  quizBox.classList.remove("active");
+  resultBox.classList.add("active");
+
+  const scoreText = document.querySelector(".score-text");
+  scoreText.textContent = `Your Score ${userScore} out of ${questions.length}`;
+
+  const circularProgress = document.querySelector(".circular-progress");
+  const progressValue = document.querySelector(".progress-value");
+  let progressStartValue = -1;
+  let progressEndValue = (userScore / questions.length) * 100;
+  let speed = 20;
+
+  let progress = setInterval(() => {
+    progressStartValue++;
+    progressValue.textContent = `${progressStartValue}%`;
+    circularProgress.style.background = `conic-gradient(#c40094 ${
+      progressStartValue * 3.6
+    }deg, rgba(255, 255, 255, 0.1) 0deg)`;
+    if (progressStartValue == progressEndValue) {
+      clearInterval(progress);
+    }
+  }, speed);
 }
